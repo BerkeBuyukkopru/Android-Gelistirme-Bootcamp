581c6ccebbb48dcaf2e9723d941946512f6a866f
Berke Büyükköprü <berkebuyukkopru06@gmail.com>
2025-05-13 11:14:40 +0300

    MVVMHilt

--- KisilerUygulamasi/app/build.gradle.kts
+++ KisilerUygulamasi/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -41,7 +43,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -52,4 +53,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
+    implementation("androidx.activity:activity-ktx:1.6.1")
+
+    implementation("com.google.dagger:hilt-android:2.56")
+    kapt("com.google.dagger:hilt-android-compiler:2.56")
 }
--- KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -14,10 +15,16 @@ import com.example.kisileruygulamasi.R
 import com.example.kisileruygulamasi.data.entity.Kisiler
 import com.example.kisileruygulamasi.databinding.FragmentAnasayfaBinding
 import com.example.kisileruygulamasi.ui.adapter.KisilerAdapter
+import com.example.kisileruygulamasi.ui.viewmodel.AnasayfaViewModel
+import com.example.kisileruygulamasi.ui.viewmodel.KisiDetayViewModel
+import com.example.kisileruygulamasi.utils.gecisYap
+import dagger.hilt.android.AndroidEntryPoint
+import kotlin.getValue
 
-
+@AndroidEntryPoint
 class AnasayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnasayfaBinding
+    private lateinit var viewModel: AnasayfaViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,30 +32,24 @@ class AnasayfaFragment : Fragment() {
         binding = FragmentAnasayfaBinding.inflate(inflater, container, false)
 
         binding.fab.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.kisiKayitGecis)
+            Navigation.gecisYap(it,R.id.kisiKayitGecis)
         }
 
-        val kisilerListesi = ArrayList<Kisiler>()
-        val k1 = Kisiler(1,"Ahmet","1111")
-        val k2 = Kisiler(2,"Zeynep","2222")
-        val k3 = Kisiler(3,"Beyza","3333")
-        kisilerListesi.add(k1)
-        kisilerListesi.add(k2)
-        kisilerListesi.add(k3)
-
-        val kisilerAdapter = KisilerAdapter(requireContext(),kisilerListesi)
-        binding.kisilerRv.adapter = kisilerAdapter
+        viewModel.kisilerListesi.observe(viewLifecycleOwner) {//Dinleme
+            val kisilerAdapter = KisilerAdapter(requireContext(),it,viewModel)
+            binding.kisilerRv.adapter = kisilerAdapter
+        }
 
         binding.kisilerRv.layoutManager = LinearLayoutManager(requireContext())
 
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextChange(newText: String): Boolean {//Harf girdikçe ve sildikçe
-                ara(newText)
+                viewModel.ara(newText)
                 return true
             }
 
             override fun onQueryTextSubmit(query: String): Boolean {//Ara buttonuna basılınca
-                ara(query)
+                viewModel.ara(query)
                 return true
             }
         })
@@ -56,12 +57,14 @@ class AnasayfaFragment : Fragment() {
         return binding.root
     }
 
-    fun ara(aramaKelimesi:String){
-        Log.e("Kişi Ara",aramaKelimesi)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel: AnasayfaViewModel by viewModels()
+        viewModel = tempViewModel
     }
 
     override fun onResume() {
         super.onResume()
-        Log.e("Kişi Anasayfaya","Dönüldü")
+        viewModel.kisileriYukle()
     }
 }
